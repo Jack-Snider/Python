c7196cfee27626c5c85a360bcdb4522391bff228
Jack Snider <choiman2021@gmail.com>
2022-08-24 21:02:43 +0900

    day 01

---
+++ HELLOPYTHON/day01/myif01.py
@@ -0,0 +1,8 @@
+
+if True:
+	print( "창" )
+
+if False:
+	print( "바보1" )
+	
+print( "바보2" )	
---
+++ myvar.py
@@ -0,0 +1,15 @@
+a = 'a'
+b = 2
+c = 1.3
+d = True
+e = False
+
+print( a + str(2) )
+
+print( c + b )
+
+print( not d )
+
+print( d and e )
+
+print( d or e )
