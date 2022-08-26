3ca0e97b6a6e6f43fdb4a9a9e3721b4ea381331e
Jack Snider <choiman2021@gmail.com>
2022-08-26 11:47:26 +0900

    day 03

---
+++ HELLOPYTHON/day03/mydef01.py
@@ -0,0 +1,22 @@
+def add( a,b ):
+    return a + b
+
+def minus( a,b ):
+    return a - b
+
+def divide( a,b ):
+    return a / b
+
+def multiply( a,b ):
+    return a * b
+
+sum = add( 5,6 )
+min = minus( 5,6 )
+div = divide( 5,6 )
+mul = multiply( 5,6 )
+
+
+print( f'sum : { sum }' )
+print( f'min : { min }' )
+print( f'div : { div }' )
+print( f'mul : { mul }' )
---
+++ HELLOPYTHON/day03/mydef03.py
@@ -0,0 +1,7 @@
+import random
+def getRandom1_9():
+    
+    return int(random.random() * 9) + 1
+
+su = getRandom1_9()
+print( f'su : { su }' )
